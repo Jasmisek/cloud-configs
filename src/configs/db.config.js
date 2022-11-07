deefe152916806cbe00d4b2a498f27b175f62067
Jasmisek <72493881+Jasmisek@users.noreply.github.com>
2022-11-07 15:15:40 +0000

    update

--- src/configs/db.config.js
+++ src/configs/db.config.js
@@ -2,7 +2,7 @@ const env = process.env
 const fs = require("fs")
 const db = {
     host: env.DB_HOST,
-    user: "root",
+    user: env.DB_USER,
     password: env.DB_PASSWORD,
     database: env.DB_NAME || "cloud_cfg",
     port: env.DB_PORT || 3306,
