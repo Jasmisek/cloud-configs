75834709222477a2e7125827766a9a57b3d09ced
Jasmisek <72493881+Jasmisek@users.noreply.github.com>
2022-11-07 15:24:48 +0000

    commited wrong thing oops

--- src/configs/db.config.js
+++ src/configs/db.config.js
@@ -1,11 +1,12 @@
+require("dotenv").config()
 const env = process.env
 const fs = require("fs")
 const db = {
     host: env.DB_HOST,
     user: env.DB_USER,
     password: env.DB_PASSWORD,
-    database: env.DB_NAME || "cloud_cfg",
-    port: env.DB_PORT || 3306,
+    database: env.DB_NAME,
+    port: env.DB_PORT,
 }
 
 module.exports = db
